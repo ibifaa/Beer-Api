27729c157f22ec4bbe2605f473deb8e95773c0a4
devibifaa <ib2link@yahoo.com>
2023-12-20 04:31:46 +0100

    updated the ui and made more request on the api

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const bear = document.getElementById("bear");
 const btn = document.getElementById("grabButton");
+const beerName = document.querySelector(".name");
+let  beerVolume = document.querySelector(".volume");
+let date = document.querySelector(".date");
+let beerAlv = document.querySelector(".alv")
 
-let beerImage = document.querySelector(".image")
+let beerImage = document.querySelector(".image");
 let beerDescription = document.querySelector(".description");
 
+document.addEventListener('DOMContentLoaded', getBear())
 btn.addEventListener("click", ()=>{
     getBear()
 })
 
-
 function getBear(){
+
     fetch("https://api.punkapi.com/v2/beers/random")
     .then(response => response.json())
     .then(beers => {
        let beer = beers[0]
-       beerImage.src = beer.image_url;
-       beerDescription.textContent = beer.description;
+      getBeerDetails(beer);
+       console.log(beer)
 
     })
     .catch(err =>{
@@ -24,4 +29,13 @@ function getBear(){
    
 }
 
-getBear();
+function getBeerDetails(beer){
+    beerImage.src = beer.image_url;
+    beerDescription.textContent = beer.description;
+    beerName.textContent = beer.name;
+    beerAlv.textContent = beer.abv;
+    beerVolume.textContent = beer.volume.value;
+    date.textContent= beer.first_brewed;
+
+
+};
